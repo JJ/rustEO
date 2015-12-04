4072900b4fe559cce3413314466a9caabb30dccf
JJ Merelo <jjmerelo@gmail.com>
2015-12-04 08:01:19 +0100

    Computes maxones

--- maxones/src/main.rs
+++ maxones/src/main.rs
@@ -1,5 +1,7 @@
 extern crate rand;
+extern crate time;
 
+use time::PreciseTime;
 use rand::Rng;
 
 fn main() {
@@ -7,21 +9,16 @@ fn main() {
     let last = 16384;
     let how_many = 10000;
     let mut rng = rand::thread_rng();
+
     while len <= last {
+        let start = PreciseTime::now();
         for i in 0..how_many {
             let this_vec: Vec<bool> = rng.gen_iter::<bool>().take(len).collect();
-            let maxones = this_vec.iter().fold(0, |acc, &item| acc + is_one(item));
-            println!("{}", maxones)
+            let maxones = this_vec.iter().fold(0, |acc, &item| acc + (if item == true { 1 } else {0}));
         }
-        println!("{}",len);
+        let end = PreciseTime::now();
+        println!("Rust-BitVector,{},{}", len,start.to(end));
         len = len*2;
     }
 }
 
-fn is_one( bit: bool ) -> i16 {
-    if  bit == true  {
-        1
-    } else {
-        0
-    }
-}
